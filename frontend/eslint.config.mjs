85aa2a0520999be2cf85c5bb4890faea23e0a916
Flynn Bundy <flynn.sbundy@gmail.com>
2025-02-02 09:41:14 +0100

    html text

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "react/no-unknown-property": "off",
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
