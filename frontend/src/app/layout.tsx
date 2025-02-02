e45cce69834abe8d86b040018e89ded6e123aa76
Flynn Bundy <flynn.sbundy@gmail.com>
2025-02-02 12:43:02 +0100

    html text

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,12 +5,29 @@ import { cn } from '@/lib/utils'
 import { WalletProvider } from '@/lib/wallet-provider'
 import { ClerkProvider } from '@clerk/nextjs'
 import '@coinbase/onchainkit/styles.css'
+import { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from "next/font/google"
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
+
+export const metadata: Metadata = {
+  title: 'Basemail',
+  description: 'Basemail',
+  icons: {
+    icon: '/b_trasnparent.png',
+  },
+  openGraph: {
+    images: '/b_trasnparent.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: '/b_trasnparent.png',
+  },
+  metadataBase: new URL('https://basemail.me'),
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
